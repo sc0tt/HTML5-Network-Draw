9492d2a343d2c30877d5ef3a7adb250f35b2c22d
sc0tt <scottynosabe@gmail.com>
2011-07-18 20:49:20 -0400

    live drawing works for two people at a time. Any more and the other viewers will see wavy lines

--- paintapp.js
+++ paintapp.js
@@ -9,14 +9,31 @@ $(document).ready(function () {
 	context.lineWidth = 5;
 	context.lineJoin = "round";
 	context.strokeStyle = "#000000";
+    var waiting = false, tempX = new Array(), tempY = new Array(), tempDrag = new Array();
 	var cX = new Array();
 	var cY = new Array();
 	var cDrag = new Array();
-
+    var lastSentID = 0;
 	var isPainting;
 	var lasti = 0;
+    var processing = false;
 	context.fillStyle = '#ffffff';
 	context.fillRect (0, 0, canvas.width, canvas.height);
+    
+    var socket = io.connect('http://radicalwhale.net:6969');
+	var sessionId;    
+    //SOCKETSSS
+    socket.on('connect', function() {
+		sessionId = socket.socket.sessionid;
+	});
+    
+    socket.on('add', function(x, y, drag) {
+        tempX.push(x);
+		tempY.push(y);
+		tempDrag.push(drag);
+        waiting = true;
+        update(); //Easier than doing newClick() a bunch of times
+	});
     
     $(window).resize(function() {
         canvas.setAttribute('width', window.innerWidth-11);
@@ -26,32 +43,59 @@ $(document).ready(function () {
     });
     
 	$('#paintapp').mousedown(function(e) {
-		var mX = e.pageX - this.offsetLeft;
-		var mY = e.pageY - this.offsetTop;
-		isPainting = true;
-		newClick(mX, mY);
-		update();
+        if(processing == false){
+            var mX = e.pageX - this.offsetLeft;
+            var mY = e.pageY - this.offsetTop;
+            isPainting = true;
+            newClick(mX, mY);
+            update();
+        }
 	})
     .mousemove(function(e) {
-		if(isPainting) {
+		if(isPainting && processing == false) {
 			newClick(e.pageX - this.offsetLeft, e.pageY - this.offsetTop, true);
-			update();
 		}
+        update();
 	})
     .mouseleave(function(e) {
 		isPainting = false;
 	})
     .mouseup(function(e) {
 		isPainting = false;
+        update();
+         //Will eventually send only new data
 	});
-	
+	/* .mouseup(function(e) {
+		isPainting = false;
+        var tX, tY, tDrag;
+        for(var i = lastSentID; i < cX.length; i++) {
+            tX = tX + cX[i] + (i < cX.length-1 ? "," : "");
+            tY = tY + cY[i] + (i < cY.length-1 ? "," : "");;
+            tDrag = tDrag + cDrag[i] + (i < cDrag.length-1 ? "," : "");;     
+        }
+        socket.emit('add', tX, tY, tDrag); //Will eventually send only new data
+	}); */
 	function newClick(x, y, isDragging)	{
-		cX.push(x);
-		cY.push(y);
-		cDrag.push(isDragging);
+        cX.push(x);
+        cY.push(y);
+        cDrag.push(isDragging);
+        socket.emit('add', x, y, isDragging);
 	}
 
 	function update(){
+        //console.log("Painting: " + isPainting + "; Waiting: " + waiting + "; Processing: " + processing);
+        if(isPainting == false && waiting == true && processing == false) {
+            processing = true;
+            cX = cX.concat(tempX);
+            cY = cY.concat(tempY);
+            cDrag = cDrag.concat(tempDrag);
+            processing = false;
+            waiting = false;
+            tempX = new Array();
+            tempY = new Array();
+            tempDrag = new Array();
+        }
+    
 		for(var i=lasti; i < cX.length; i++) {	
 			context.beginPath();
 			context.lineWidth = 5;
@@ -68,12 +112,29 @@ $(document).ready(function () {
 		}
 		lasti = cX.length;
 	}
-	function clear() {
+	function clear() { //never used, for debugging
 		context.clearRect(0, 0, canvas.width, canvas.height);
 		var w = canvas.width;
 		canvas.width = 1;
 		canvas.width = w;
 		context.fillRect (0, 0, canvas.width, canvas.height);
 	}
+    
+    window.requestAnimFrame = (function(){
+        return window.requestAnimationFrame ||
+        window.webkitRequestAnimationFrame ||
+        window.mozRequestAnimationFrame ||
+        window.oRequestAnimationFrame ||
+        window.msRequestAnimationFrame ||
+        function(/* function */ callback, /* DOMElement */ element){
+            window.setTimeout(callback, 1000 / 60);
+        };
+    })();	
+
+    (function loop() {
+        update();
+        requestAnimFrame(loop, canvas);
+    })();
+    
 });
 

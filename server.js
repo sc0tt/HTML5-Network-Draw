5749dd3997bd277d165edb8868ea24ba7fc42215
Sc0tt <scottynosabe@gmail.com>
2012-03-27 23:01:51 -0400

    Clears canvas image between each session (woops) Crosshair for drawing Better, faster method for saving images

--- server.js
+++ server.js
@@ -12,43 +12,49 @@ io.sockets.on('connection', function(socket) {
     socket.emit('add', history[i]);
   }
   socket.on('clear', function(pass) {
-		if(pass == adminPW) {
-      history = [];
-			io.sockets.emit('clearUser');
-		}
-	});
-	socket.on('add', function(data) {
+    if(pass == adminPW) {
+      io.sockets.emit('clearUser');
+    }
+  });
+  socket.on('add', function(data) {
     history.push(data);
-		socket.broadcast.emit('add', data);
-	});
+    drawToCanvas(data);
+    socket.broadcast.emit('add', data);
+  });
 });
 var minutes = 5 * 60 * 1000;
-setInterval(function() {
-  for(var i=0; i < history.length; i++) {	
-      context.lineWidth = 5;
-      context.lineJoin = "round";
-      context.strokeStyle = "#000000";
-      var currLine = JSON.parse(history[i]);
-      for(var z = 1; z < currLine.x.length; z++) {   
-        context.beginPath();
-        console.log(currLine);
-        if(currLine.drag[z]){
-          context.moveTo(currLine.x[z-1], currLine.y[z-1]);
-        }else{
-          context.moveTo(currLine.x[z-1], currLine.y[z]);
-        }
-        context.lineTo(currLine.x[z], currLine.y[z]);
-        context.closePath();
-        context.stroke();
-      }
-      
-    }
-    var out = fs.createWriteStream('./img/last.png'),
-    stream = canvas.createPNGStream();
 
-    stream.on('data', function(chunk){
-      out.write(chunk);
-    });
+function drawToCanvas(data)
+{
+  context.lineWidth = 5;
+  context.lineJoin = "round";
+  context.strokeStyle = "#000000";
+  var currLine = JSON.parse(data);
+  for(var z = 1; z < currLine.x.length; z++) {   
+    context.beginPath();
+    if(currLine.drag[z]){
+      context.moveTo(currLine.x[z-1], currLine.y[z-1]);
+    }else{
+      context.moveTo(currLine.x[z-1], currLine.y[z]);
+    }
+    context.lineTo(currLine.x[z], currLine.y[z]);
+    context.closePath();
+    context.stroke();
+  }
+}
+
+setInterval(function() {
+  fs.unlinkSync('./img/last.png');
+  var out = fs.createWriteStream('./img/last.png'),
+  stream = canvas.toDataURL().replace(/^data:image\/png;base64,/,""),
+  buffer = new Buffer(stream, 'base64');
+  fs.writeFile('./img/last.png', buffer, function(error) {
+    console.log(error);
+  });
+  context.save();
+  context.setTransform(1, 0, 0, 1, 0, 0);
+  context.clearRect(0, 0, canvas.width, canvas.height);
+  context.restore();
   history = [];
   io.sockets.emit('clearUser');
 }, minutes);
